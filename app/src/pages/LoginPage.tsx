1feb9dad17640fa2c05d759404037d904864b1b0
Josh <josh@bemoty.dev>
2021-07-22 16:31:29 +0200

    login: added beta notice

--- app/src/pages/LoginPage.tsx
+++ app/src/pages/LoginPage.tsx
@@ -12,6 +12,11 @@ export default function LoginPage() {
 			</Helmet>
 			<Title>{t('pages.main.title')}</Title>
 			<Subheading className="mt-1">{t('pages.main.subtitle')}</Subheading>
+			<p className="text-opacity-70 my-2 w-8/12 mx-auto">
+				<span className="font-bold">Please note:</span> This app is
+				still in closed beta mode. You cannot use the login button below
+				unless you have been invited by Josh or Nicolaus.
+			</p>
 			<SpotifyLoginButton className="mt-5 mx-auto" link="/login">
 				{t('pages.main.login')}
 			</SpotifyLoginButton>
